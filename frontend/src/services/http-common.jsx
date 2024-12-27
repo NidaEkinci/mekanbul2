361053bc0ad64d4c2f971f78be161edeb344bc95
Nida EKİNCİ <l21110120191@stud.sdu.edu.tr>
2024-12-27 16:04:43 +0300

    e

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekanbul-26aralik.vercel.app/api",
+  baseURL: "https://mekanbul2-gold.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
